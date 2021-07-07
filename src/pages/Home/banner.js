50a5a3e1e2b9123422371c6c0a8af8563cfa9421
Manoj Ponugoti <manoj@fidisys.com>
2021-07-07 10:57:37 +0530

    changes done for cms test

--- src/pages/Home/banner.js
+++ src/pages/Home/banner.js
@@ -92,7 +92,7 @@ const Banner = () => {
                                 </DialogActions>
                                 <DialogContent id="popup">
                                     <iframe width="900" height="500"
-                                        src="https://www.youtube.com/embed/pR195-H96Eo?autoplay=1&mute=1" id="popvideo" title="videopopup">
+                                        src="https://www.youtube.com/embed/pR195-H96Eo?autoplay=1&mute=0" id="popvideo" title="videopopup">
                                     </iframe>
                                 </DialogContent>
                             </Dialog>
