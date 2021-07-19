3f62570dd7f052a6490ac4d3e4c30b34e4e1c5c4
ManojDev <manoj@fidisys.com>
2021-07-19 17:36:36 +0530

    changes done

--- src/cms/preview-templates/homeBlogsPreview.js
+++ src/cms/preview-templates/homeBlogsPreview.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Blogs from "../../pages/Home/blogs";
+import { Blogs } from "../../pages/Home/blogs";
 
 const homeBlogsPreview = ({ entry, getAsset }) => {
 
